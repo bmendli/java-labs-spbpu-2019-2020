e30e8b36de3d0b62a9fe2489a32dbbbff2dcc527
Maschenko Bogdan 13534/4 <mashchenko.bogdan2016@gmail.com>
2019-10-01 23:49:14 +0300

    first impl of lab5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,19 @@
+import lab5.FilePropertiesWorker;
+
+import java.io.FileNotFoundException;
+
 public class Main {
+    public static void main(String[] args) {
+        try {
+            FilePropertiesWorker filePropertiesWorker = new FilePropertiesWorker("pro.properties");
+            filePropertiesWorker.parse();
+            filePropertiesWorker.getProperties().forEach(
+                (String key, String value) -> {
+                    System.out.println(key + " " + value);
+                }
+            );
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
